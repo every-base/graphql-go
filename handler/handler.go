545c4d1b46e2cd9c67a63540d349fbd6b2df42bc
tdd28 <tdd2810@gmail.com>
2025-02-16 17:17:29 +0700

    feat: HTTP handler

---
+++ handler/handler.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/every-base/graphql-go"
+)
+
+type HandlerOption func(h *Handler)
+
+var _ http.Handler = (*Handler)(nil)
+
+func New(schema *graphql.Schema, opts ...HandlerOption) *Handler {
+	h := &Handler{schema: schema}
+	for _, opt := range opts {
+		opt(h)
+	}
+	return h
+}
+
+type Handler struct {
+	schema *graphql.Schema
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		h.exec(w, r)
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
+func (h *Handler) exec(w http.ResponseWriter, r *http.Request) {
+	var params struct {
+		Query         string                 `json:"query"`
+		OperationName string                 `json:"operationName"`
+		Variables     map[string]interface{} `json:"variables"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	resp := h.schema.Exec(r.Context(), params.Query, params.OperationName, params.Variables)
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(resp)
+}
